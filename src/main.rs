a51cc9ad60a940d3e3f838824eaf55abcb72967b
Emanuel Geromin <egeromin@gmail.com>
2018-06-22 12:33:07 -0400

    Added positions of the startpoints.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 extern crate pxl;
 use pxl::*;
 
-
 const RACETRACK: &[u8] = include_bytes!("../racetrack.values");
 const EPISODE: &[u8] = include_bytes!("../episode.values");
 const TICKS_PER_SECOND: u32 = 60;
 // const NUM_EPISODES: usize = EPISODE.len() / 2;
 
-
-#[derive(Copy,Clone)]
+#[derive(Copy, Clone, PartialEq)]
 enum TrackPoint {
     Boundary,
     End,
@@ -17,11 +15,8 @@ enum TrackPoint {
 }
 
 use TrackPoint::*;
-
-
 impl TrackPoint {
-
-    fn from_byte(byte:u8) -> TrackPoint {
+    fn from_byte(byte: u8) -> TrackPoint {
         match byte {
             0 => Boundary,
             1 => End,
@@ -33,34 +28,64 @@ impl TrackPoint {
 
     fn pixel(self) -> Pixel {
         match self {
-            Boundary => Pixel{red:0,green:0,blue:0},
-            End => Pixel{red:0,green:255,blue:0},
-            Beginning => Pixel{red:255,green:0,blue:0},
-            Empty => Pixel{red:255,green:255,blue:255},
+            Boundary => Pixel {
+                red: 0,
+                green: 0,
+                blue: 0,
+            },
+            End => Pixel {
+                red: 0,
+                green: 255,
+                blue: 0,
+            },
+            Beginning => Pixel {
+                red: 255,
+                green: 0,
+                blue: 0,
+            },
+            Empty => Pixel {
+                red: 255,
+                green: 255,
+                blue: 255,
+            },
         }
     }
 }
 
 struct Game {
     track: Vec<TrackPoint>,
+    start_positions: Vec<usize>,
     episode_num: usize,
-    tick: u32
+    tick: u32,
 }
 
 impl Program for Game {
     fn new() -> Game {
         assert_eq!(RACETRACK.len(), 2500);
-        let track = RACETRACK.iter().cloned().map(TrackPoint::from_byte).collect();
-        Game{track, episode_num: 0, tick: 0}
+        let track = RACETRACK
+            .iter()
+            .cloned()
+            .map(TrackPoint::from_byte)
+            .collect::<Vec<TrackPoint>>();
+        let start_positions = (0 as usize..2500 as usize)
+            .into_iter()
+            .filter(|&i| track[i] == Beginning)
+            .collect();
+        Game {
+            track,
+            start_positions,
+            episode_num: 0,
+            tick: 0,
+        }
     }
 
     fn render(&mut self, pixels: &mut [Pixel]) {
         for x in 0..50 {
             for y in 0..50 {
-                let pixel = self.track[y*50 + x].pixel();
+                let pixel = self.track[y * 50 + x].pixel();
                 for i in 0..5 {
                     for j in 0..5 {
-                        pixels[(5*y+j+3)*256 + 5*x + i+3] = pixel;
+                        pixels[(5 * y + j + 3) * 256 + 5 * x + i + 3] = pixel;
                     }
                 }
             }
@@ -68,26 +93,34 @@ impl Program for Game {
     }
 
     fn tick(&mut self, _events: &[Event]) {
-
         self.tick += 1;
 
         if self.tick % TICKS_PER_SECOND == 0 {
-
             // let (x, y) = EPISODE[2*self.episode_num: 2*self.episode_num+1]
-            let x =  (EPISODE[2*self.episode_num] - 1) as usize;
-            let y =  (EPISODE[2*self.episode_num+1] -1) as usize;
+            let x = (EPISODE[2 * self.episode_num] - 1) as usize;
+            let y = (EPISODE[2 * self.episode_num + 1] - 1) as usize;
 
-            self.track[50*y + x] = Boundary;
+            self.track[50 * y + x] = Boundary;
             self.episode_num = (self.episode_num + 1) % (EPISODE.len() / 2);
 
             if self.episode_num == 0 {
-                self.track = RACETRACK.iter().cloned().map(TrackPoint::from_byte).collect();
+                self.track = RACETRACK
+                    .iter()
+                    .cloned()
+                    .map(TrackPoint::from_byte)
+                    .collect();
             }
 
             self.tick = 0;
         }
-
     }
+
+    // fn next_state(&mut self) {
+    //     return self.random_start();
+    // }
+
+    // fn random_start(&mut self) {
+    // }
 }
 
 fn main() {
